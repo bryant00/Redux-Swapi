c104fb335911948031a6ca51832d7144ba0d8dc0
Kizmet <bryantpatton@gmail.com>
2019-06-19 13:53:52 -0700

    api setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,22 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+import axios from 'axios';
+
+export const FETCH_START = 'FETCH_START';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAIL = 'FETCH_FAIL';
+
+export const getCharacters = () => dispatch => {
+  dispatch({ type: FETCH_START });
+  axios
+    .get("https://swapi.co/api/people/")
+    .then(({data}) => {
+      dispatch({ type: FETCH_SUCCESS, payload: data.results });
+    })
+    .catch(err => {
+        console.log(err);
+        dispatch({type: FETCH_FAIL,payload: err})
+    });
+};
+
